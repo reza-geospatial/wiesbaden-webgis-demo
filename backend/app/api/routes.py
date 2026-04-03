e268c84060b3d7e705744a4313d3c46c6b954727
Reza Mohammadi <mohammadi@iz.de>
2026-04-03 17:09:57 +0200

    feat(api): add BBOX filtering and zoom-based geometry simplification

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -2,43 +2,6 @@ from fastapi import APIRouter
 from sqlalchemy import create_engine, text
 import os
 from dotenv import load_dotenv
-
-router = APIRouter()
-
-# =========================
-# DB CONFIG
-# =========================
-
-load_dotenv()
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-engine = create_engine(
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
-
-# =========================
-
-
-@router.get("/green")
-def get_green_areas(limit: int = 100):
-
-    query = text(f"""
-        SELECT 
-            id,
-            ST_AsGeoJSON(geometry) AS geometry
-        FROM green_areas
-        LIMIT :limit
-    """)
-
-    from fastapi import APIRouter
-from sqlalchemy import create_engine, text
-import os
-from dotenv import load_dotenv
 import json
 
 router = APIRouter()
@@ -67,15 +30,25 @@ def get_green_areas(
     minx: float,
     miny: float,
     maxx: float,
-    maxy: float
+    maxy: float,
+    zoom: int
     ):
+    
+    if zoom < 12:
+        tolerance = 0.0001
+    elif zoom < 14:
+        tolerance = 0.00005
+    else:
+        tolerance = 0.00001
 
     query = text("""
         SELECT 
             id,
             landuse,
             leisure,
-            ST_AsGeoJSON(geometry) AS geometry,
+            ST_AsGeoJSON(
+                 ST_SimplifyPreserveTopology(geometry, :tolerance)
+                 ) AS geometry,
             ST_Area(ST_Transform(geometry, 25832)) AS area
         FROM green_areas
         WHERE geometry && ST_MakeEnvelope(:minx, :miny, :maxx, :maxy, 4326)
@@ -86,7 +59,8 @@ def get_green_areas(
             "minx": minx,
             "miny": miny,
             "maxx": maxx,
-            "maxy": maxy
+            "maxy": maxy,
+            "tolerance": tolerance
             })
 
         features = []
